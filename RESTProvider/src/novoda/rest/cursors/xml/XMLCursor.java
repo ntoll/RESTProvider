0cec9a86e3fc0e63f1174e84be7159fede83a4d2
Carl-Gustaf Harroch <carl@novoda.com>
2010-01-03 19:25:00 +0000

    XMLCursor working with int, long and so forth
    Playing with buildr a bit and changing the XML cursor so it works with orther format then String.

--- RESTProvider/src/novoda/rest/cursors/xml/XMLCursor.java
+++ RESTProvider/src/novoda/rest/cursors/xml/XMLCursor.java
@@ -11,7 +11,6 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.util.EntityUtils;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
-import org.dom4j.Element;
 import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 
@@ -21,43 +20,85 @@ import android.util.Log;
 public abstract class XMLCursor extends AbstractCursor implements QueryHandler<XMLCursor> {
 
     private static final String TAG = XMLCursor.class.getSimpleName();
+
     private Document document;
 
     @Override
     public int getCount() {
-        int ret = Integer.parseInt(((Node)document.selectSingleNode(getCountXPath())).getStringValue());
+        int ret = 0;
+        try {
+            ret = Integer.parseInt(((Node)document.selectSingleNode(getCountXPath()))
+                    .getStringValue());
+        } catch (NumberFormatException e) {
+            Log.e(TAG, "Can't parse the count for: " + document.asXML() + " using "
+                    + getCountXPath());
+        }
         return ret;
     }
-    
+
     @Override
     public double getDouble(int column) {
-        return 0;
+        double ret = 0;
+        try {
+            ret = Double.parseDouble(((Node)document.selectSingleNode(getXPath(mPos,
+                    getColumnName(column)))).getStringValue());
+        } catch (NumberFormatException e) {
+            Log.e(TAG, "Can't parse double for: " + document.asXML() + " using " + getCountXPath());
+        }
+        return ret;
     }
 
     @Override
     public float getFloat(int column) {
-        return 0;
+        float ret = 0;
+        try {
+            ret = Float.parseFloat(((Node)document.selectSingleNode(getXPath(mPos,
+                    getColumnName(column)))).getStringValue());
+        } catch (NumberFormatException e) {
+            Log.e(TAG, "Can't parse float for: " + document.asXML() + " using " + getCountXPath());
+        }
+        return ret;
     }
 
     @Override
     public int getInt(int column) {
-        return 0;
+        int ret = 0;
+        try {
+            ret = Integer.parseInt(((Node)document.selectSingleNode(getXPath(mPos,
+                    getColumnName(column)))).getStringValue());
+        } catch (NumberFormatException e) {
+            Log.e(TAG, "Can't parse int for: " + document.asXML() + " using " + getCountXPath());
+        }
+        return ret;
     }
 
     @Override
     public long getLong(int column) {
-        return 0;
+        long ret = 0;
+        try {
+            ret = Long.parseLong(((Node)document.selectSingleNode(getXPath(mPos,
+                    getColumnName(column)))).getStringValue());
+        } catch (NumberFormatException e) {
+            Log.e(TAG, "Can't parse long for: " + document.asXML() + " using " + getCountXPath());
+        }
+        return ret;
     }
 
     @Override
     public short getShort(int column) {
-        return 0;
+        short ret = 0;
+        try {
+            ret = Short.parseShort(((Node)document.selectSingleNode(getXPath(mPos,
+                    getColumnName(column)))).getStringValue());
+        } catch (NumberFormatException e) {
+            Log.e(TAG, "Can't parse short for: " + document.asXML() + " using " + getCountXPath());
+        }
+        return ret;
     }
 
     @Override
     public String getString(int column) {
-        String ret = ((Node)document.selectSingleNode(
-                getXPath(mPos, getColumnName(column))))
+        String ret = ((Node)document.selectSingleNode(getXPath(mPos, getColumnName(column))))
                 .getStringValue();
         return ret;
     }
@@ -78,10 +119,11 @@ public abstract class XMLCursor extends AbstractCursor implements QueryHandler<X
         } catch (DocumentException e) {
             e.printStackTrace();
             Log.e(TAG, "an error occured in handleResponse", e);
-        } 
+        }
         return this;
     }
-    
+
     public abstract String getXPath(int row, String column);
+
     public abstract String getCountXPath();
 }
